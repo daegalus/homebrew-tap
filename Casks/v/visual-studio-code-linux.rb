26d52f402051f8633ebc15afa7a4a3502e811eea
Yulian Kuncheff <yulian@kuncheff.com>
2025-09-16 23:47:11 +0200

    Fix style

--- Casks/v/visual-studio-code-linux.rb
+++ Casks/v/visual-studio-code-linux.rb
@@ -2,16 +2,9 @@ cask "visual-studio-code-linux" do
   arch arm: "arm64", intel: "x64"
   os linux: "linux"
 
-    version "1.104.0"
-    sha256 arm64_linux:  "92d29b2206c5d5e979c4707e012dee3f7a37e47d9d221509e0d1cd0a8421237b",
-           x86_64_linux: "0019db2e217c00a0a5b068e11dbff22b88c3dd3b7ccbf35d6f189c4a4a7e1dbb"
-
-    livecheck do
-      url "https://update.code.visualstudio.com/api/update/#{os}-#{arch}/stable/latest"
-      strategy :json do |json|
-        json["productVersion"]
-      end
-    end
+  version "1.104.0"
+  sha256 arm64_linux:  "92d29b2206c5d5e979c4707e012dee3f7a37e47d9d221509e0d1cd0a8421237b",
+         x86_64_linux: "0019db2e217c00a0a5b068e11dbff22b88c3dd3b7ccbf35d6f189c4a4a7e1dbb"
 
   url "https://update.code.visualstudio.com/#{version}/#{os}-#{arch}/stable"
   name "Microsoft Visual Studio Code"
@@ -19,15 +12,24 @@ cask "visual-studio-code-linux" do
   desc "Open-source code editor"
   homepage "https://code.visualstudio.com/"
 
+  livecheck do
+    url "https://update.code.visualstudio.com/api/update/#{os}-#{arch}/stable/latest"
+    strategy :json do |json|
+      json["productVersion"]
+    end
+  end
+
   auto_updates true
 
   binary "#{staged_path}/VSCode-linux-#{arch}/bin/code"
   binary "#{staged_path}/VSCode-linux-#{arch}/bin/code-tunnel"
-  artifact "#{staged_path}/VSCode-linux-#{arch}/code.desktop", target: "#{ENV["HOME"]}/.local/share/applications/code.desktop"
-  artifact "#{staged_path}/VSCode-linux-#{arch}/code-url-handler.desktop", target: "#{ENV["HOME"]}/.local/share/applications/code-url-handler.desktop"
+  artifact "#{staged_path}/VSCode-linux-#{arch}/code.desktop",
+           target: "#{Dir.home}/.local/share/applications/code.desktop"
+  artifact "#{staged_path}/VSCode-linux-#{arch}/code-url-handler.desktop",
+           target: "#{Dir.home}/.local/share/applications/code-url-handler.desktop"
 
   preflight do
-    FileUtils.mkdir_p "#{ENV["HOME"]}/.local/share/applications"
+    FileUtils.mkdir_p "#{Dir.home}/.local/share/applications"
     File.write("#{staged_path}/VSCode-linux-#{arch}/code.desktop", <<~EOS)
       [Desktop Entry]
       Name=Visual Studio Code
@@ -73,7 +75,6 @@ cask "visual-studio-code-linux" do
       Keywords=vscode;
     EOS
   end
-
 
   # zap trash: [
   #   "#{ENV["HOME"]}/.config/Code",
