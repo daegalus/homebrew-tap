b490e90997055def447aae841a616fe4eeacc243
Yulian Kuncheff <670212+daegalus@users.noreply.github.com>
2026-04-14 04:03:24 +0000

    netbird-ui-linux 0.68.2

--- Casks/netbird-ui-linux.rb
+++ Casks/netbird-ui-linux.rb
@@ -1,6 +1,6 @@
 cask "netbird-ui-linux" do
-  version "0.65.2"
-  sha256 "68a5deb06cd08b7f70e888c192acb31ddb4cfe352c2a82b31d030a67e07b3c2c"
+  version "0.68.2"
+  sha256 "66d68e42ea047aaae3042b742681ae5257f89606004568546225be0e2ada22ee"
 
   url "https://github.com/netbirdio/netbird/releases/download/v#{version}/netbird-ui-linux_#{version}_linux_amd64.tar.gz"
   name "Netbird UI for Linux"
